3e05ddd888222a0c944ca1e71e05f3167069299a
tech-admins <tech-admins@appsforbharat.com>
2024-07-03 17:20:50 +0530

    added vectorstore local save

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env (especially openai api key)
 
-st.title("RockyBot: News Research Tool 📈")
-st.sidebar.title("News Article URLs")
+st.title("Article Research Tool")
+st.sidebar.title("Article URLs")
 
 urls = []
 for i in range(3):
